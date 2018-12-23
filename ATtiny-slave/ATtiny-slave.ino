b42f9041245e7ae5b40d7eba868bece124e8259e
Vit Petrik <vit.petrik@gmail.com>
2018-12-23 17:29:29 +0100

    Přímá operace s porty

--- ATtiny-slave/ATtiny-slave.ino
+++ ATtiny-slave/ATtiny-slave.ino
@@ -2,6 +2,7 @@
 //SCL = pin 7
 //SDA = pin 5
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
 
 #include <QTouchADCTiny.h>
 #include <TinyWireS.h>
@@ -16,31 +17,34 @@ int ref = 0;
 int value;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  asm (" ldi PORTB1, 1 ");
+  DDRB = DDRB | B00000010;
+  PORTB = PORTB & B11111101;
   delay(50);
   QTouchADCTiny.init();
-  
+
   TinyWireS.begin(I2C_SLAVE_ADDRESS);
   TinyWireS.onReceive(receiveEvent);
   TinyWireS.onRequest(requestEvent);
 }
 
 void loop() {
- 
+
 }
 
+byte pwmValue;
 //když master pošle data, tak je hodíme na LEDku jako PWM
 void receiveEvent(uint8_t byty)
 {
-  byte pwmValue = TinyWireS.receive();
-  if ( pwmValue == 0 ){
-    asm (" ldi PORTB1, 1 ");
+  pwmValue = TinyWireS.receive();
+  if ( pwmValue == 0 ) {
+    cbi(TCCR0A, COM0B1);
+    PORTB = PORTB & B11111101;
   }
-  else if ( pwmValue == 255 ){
-    asm (" ldi PORTB1, 1 ");
+  else if ( pwmValue == 255 ) {
+    cbi(TCCR0A, COM0B1);
+    PORTB = PORTB | B00000010;
   }
-  else{
+  else {
     sbi(TCCR0A, COM0B1);
     OCR0B = pwmValue;
   }
@@ -48,7 +52,7 @@ void receiveEvent(uint8_t byty)
 
 //když si master vyžádá data, tak mu pošleme číslo value jako dva byty
 void requestEvent()
-{ 
+{
   value = QTouchADCTiny.sense(sensePin, refPin, 1);
   TinyWireS.send(highByte(value));
   TinyWireS.send(lowByte(value));
