39da1dd1ca2a5f7f2a95382d6400c5e03d3e4e2e
Vit Petrik <vit.petrik@gmail.com>
2018-12-23 15:21:37 +0100

    MQTT support

--- ESP32 - master/src/main.cpp
+++ ESP32 - master/src/main.cpp
@@ -1,48 +1,86 @@
 //Vít Petřík@2018
 #include <Arduino.h>
 #include <Wire.h>
+#include <PubSubClient.h>
+#include <WiFi.h>
 
-boolean turnOn = false;
-long millisOn = -1000;
-int threshold = 700;
-int interval = 1000;
-int value;
-int lastValue;
-long millisFPS = 0;
-int loopcounter = 0;
-long ref = 0;
-byte PWMvalue = 0;
+#define SSID "💩💩💩🦄😵🏳‍🌈"
+#define PASS "un1corn666"
+#define MQTT "10.10.10.19"
+#define MQTTport "1883"
 
-int readI2C(int address, int bytes){
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  if (length == 1)
+  {
+    writeI2C(0x04, payload[0]);
+  }
+}
+
+void reconnect()
+{
+
+  while (!client.connected())
+  {
+    String clientId = "0x04";
+    clientId += String(random(0xffff), HEX);
+
+    if (client.connect(clientId.c_str()))
+    {
+      client.subscribe("0x04/pwm");
+    }
+    else
+    {
+      delay(5000);
+    }
+  }
+}
+
+int readI2C(int address, int bytes)
+{
   int x = 0;
-  Wire.requestFrom(address, bytes);    //zažádá 2 byty na adrese 0x04
-  x = Wire.read();      
+  Wire.requestFrom(address, bytes); //zažádá 2 byty na adrese 0x04
+  x = Wire.read();
   x = x << 8;
-  x += Wire.read();         //uloží hodnotu do INTu po 2 bytech ( bitový posun dolevo o 8 )
+  x += Wire.read(); //uloží hodnotu do INTu po 2 bytech ( bitový posun dolevo o 8 )
   return x;
 }
- 
-void setup() {
-  delay(500);
-  Wire.begin(22,23);
-  Serial.begin(230400);
-  Wire.beginTransmission(0x04);
-  Wire.write(0);       
-  Wire.endTransmission();   
+
+void writeI2C(int address, byte data)
+{
+  Wire.beginTransmission(address);
+  Wire.write(data);
+  Wire.endTransmission();
 }
 
-void loop() {
+void setup()
+{
+  delay(500);
+  Wire.begin(22, 23);
+  Serial.begin(230400);
+  writeI2C(0x04, 0);
+  WiFi.begin(SSID, PASS);
 
-    value = readI2C(0x04, 2);
-  Serial.println(value);        //pošle hodnotu na serial... jen tak pro srandu 
-  if(value > threshold){        //pokud hodnota přesáhne threshold pošle 255 na zařízení a uloží čas ( millisOn )
-         Wire.beginTransmission(0x04);
-     Wire.write(255);       
-     Wire.endTransmission();   }
-     else{
-            Wire.beginTransmission(0x04);
-     Wire.write(0);       
-     Wire.endTransmission();   
-     }
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
 
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop()
+{
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+  value = readI2C(0x04, 2);
+  client.publish("0x04/touch", value);
 }
