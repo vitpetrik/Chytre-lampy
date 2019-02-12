651621e6a0fae44d8808e639c160da0e5b083f74
Vit Petrik <vit.petrik@gmail.com>
2019-02-12 21:58:22 +0100

    Úprava názvů funkcí

--- ESP32 - master/src/lamp.h
+++ ESP32 - master/src/lamp.h
@@ -2,16 +2,23 @@
 #include <Wire.h>
 
 //přečte hodnotu z dotykového čidla
-int readTouch(int address)
+uint8_t readTouch(int address)
 {
-  static uint8_t x = 0;
   Wire.requestFrom(address, 1);
-  x = Wire.read();
-  //x *= 4;
-  // x = x << 8;
-  // x += Wire.read();
-  //Serial.println(x);
-  return x;
+  return Wire.read();
+}
+
+//přečte souřadnice a vrátí je v poli [ X, Y ]
+uint8_t *readPosition(uint8_t address)
+{
+  static uint8_t data[2];
+  Wire.beginTransmission(address);
+  Wire.write(0x05);
+  Wire.endTransmission();
+  Wire.requestFrom(address, 2);
+  data[0] = Wire.read();
+  data[1] = Wire.read();
+  return data;
 }
 
 //zapíše PWM hodnotu na I2C
@@ -49,7 +56,7 @@ void writeFade(uint8_t address, boolean fade)
 }
 
 //zapíše novou I2C adresu
-void writeAddress(uint8_t address, uint8_t newAddress)
+void writeI2CAddress(uint8_t address, uint8_t newAddress)
 {
   if (newAddress < 128)
   {
@@ -61,26 +68,13 @@ void writeAddress(uint8_t address, uint8_t newAddress)
 }
 
 //zapíše souřadnice X a Y do ATtiny
-void writeGPS(uint8_t address, uint8_t X, uint8_t Y)
+void writePosition(uint8_t address, uint8_t X, uint8_t Y)
 {
   Wire.beginTransmission(address);
   Wire.write((uint8_t)0x04);
   Wire.write(X);
   Wire.write(Y);
   Wire.endTransmission();
-}
-
-//přečte souřadnice a vrátí je v poli [ X, Y ]
-uint8_t *readLocation(uint8_t address)
-{
-  static uint8_t data[2];
-  Wire.beginTransmission(address);
-  Wire.write(0x05);
-  Wire.endTransmission();
-  Wire.requestFrom(address, 2);
-  data[0] = Wire.read();
-  data[1] = Wire.read();
-  return data;
 }
 
 void writeSample(uint8_t address, uint8_t sample){
@@ -90,18 +84,27 @@ void writeSample(uint8_t address, uint8_t sample){
   Wire.endTransmission();
 }
 
-void autonomus(uint8_t address, boolean autonomus)
+void writeMode(uint8_t address, uint8_t Mode) {
+  Wire.beginTransmission(address);
+  Wire.write(0x0C);
+  Wire.write(Mode);
+  Wire.endTransmission();
+}
+
+void writeThreshold(uint8_t address, uint8_t thres)
 {
   Wire.beginTransmission(address);
-  Wire.write(0x06);
-  if (autonomus)
-  {
-    Wire.write(0xFF);
-  }
-  else
-  {
-    Wire.write(0x00);
-  }
+  Wire.write(0x09);
+  Wire.write(thres);
+  Wire.endTransmission();
+}
+
+void autonomusInterval(uint8_t address, int inter)
+{
+  Wire.beginTransmission(address);
+  Wire.write(0x0A);
+  Wire.write(highByte(inter));
+  Wire.write(lowByte(inter));
   Wire.endTransmission();
 }
 
@@ -118,22 +121,5 @@ void autonomusLow(uint8_t address, uint8_t PWM)
   Wire.beginTransmission(address);
   Wire.write(0x08);
   Wire.write(PWM);
-  Wire.endTransmission();
-}
-
-void autonomusThreshold(uint8_t address, uint8_t thres)
-{
-  Wire.beginTransmission(address);
-  Wire.write(0x09);
-  Wire.write(thres);
-  Wire.endTransmission();
-}
-
-void autonomusInterval(uint8_t address, int inter)
-{
-  Wire.beginTransmission(address);
-  Wire.write(0x0A);
-  Wire.write(highByte(inter));
-  Wire.write(lowByte(inter));
   Wire.endTransmission();
 }
