3b0f664eb034de369d1b2164969beae6b46d0687
ullmannV <46501870+ullmannV@users.noreply.github.com>
2019-02-17 20:09:29 +0100

    Update main.cpp

--- ESP32 - master/src/main.cpp
+++ ESP32 - master/src/main.cpp
@@ -1,6 +1,6 @@
-//Vít Petřík@2018 + Vojta Ullmann@2019
+//Vít Petřík@2018
 /*
-Máme úžasný a vysoce funkční kódy, omluvte  prehlednost :(
+Mám úžasný a vysoce funkční kódy, omluvte  prehlednost :(
 */
 #include <Arduino.h>
 #include <Wire.h>
@@ -12,17 +12,258 @@ Máme úžasný a vysoce funkční kódy, omluvte  prehlednost :(
 #include "Adafruit_SI1145.h"
 #include <lamp.h>
 #include <math.h>
-// TODO FreeRTOS knihovny? 
-//#include <queue.h>
+
 #define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme;
+Adafruit_SI1145 uv = Adafruit_SI1145();
+
+// Inicializace fronty pro ukladani souradnic
+QueueHandle_t queueX = xQueueCreate(127, sizeof(uint8_t));
+QueueHandle_t queueY = xQueueCreate(127, sizeof(uint8_t));
+
+SemaphoreHandle_t i2c_mutex = xSemaphoreCreateMutex();
+
+uint8_t lampCount = 0;
+uint8_t triggerY = 0;
+uint8_t triggerX = 0;
+uint8_t triggerCount = 0;
+uint8_t low = 5;
+uint8_t high = 255;
+uint16_t interval = 5000;
+uint8_t radius = 1;
+
+void lampTrigger(void *parameters)
+{
+}
+
+void lamp(void *parameters)
+{
+  uint8_t address = int(parameters);
+  uint8_t value = 0;
+  uint8_t lastValue = 0;
+  uint8_t pos[2];
+  unsigned long onMillis = 0;
+  bool on = false;
+
+  while (true)
+  {
+    if (xSemaphoreTake(i2c_mutex, 1 / portTICK_PERIOD_MS))
+    {
+      Wire.beginTransmission(address);
+      if (Wire.endTransmission() != 0)
+      {
+        xSemaphoreGive(i2c_mutex);
+        vTaskDelete(NULL);
+      }
+      xSemaphoreGive(i2c_mutex);
+      break;
+    }
+  }
+
+  lampCount++;
+
+  delay(10);
+
+  while (true)
+  {
+    if (xSemaphoreTake(i2c_mutex, 1 / portTICK_PERIOD_MS))
+    {
+      writeMode(address, 1);
+      delayMicroseconds(1000);
+      writeSpeed(address, 15);
+      delayMicroseconds(1000);
+      writeSample(address, 20);
+      delayMicroseconds(1000);
+      writeThreshold(address, 80);
+      delayMicroseconds(1000);
+      autonomusHigh(address, 255);
+      delayMicroseconds(1000);
+      autonomusLow(address, 5);
+      delayMicroseconds(1000);
+      autonomusInterval(address, 5000);
+      xSemaphoreGive(i2c_mutex);
+      break;
+    }
+  }
+
+  delay(10);
+
+  while (true)
+  {
+    if (xSemaphoreTake(i2c_mutex, 1 / portTICK_PERIOD_MS))
+    {
+      writePWM(address, 255);
+      delayMicroseconds(500);
+      xSemaphoreGive(i2c_mutex);
+      break;
+    }
+  }
+
+  delay(1000);
+
+  while (true)
+  {
+    if (xSemaphoreTake(i2c_mutex, 1 / portTICK_PERIOD_MS))
+    {
+      writePWM(address, 5);
+      delayMicroseconds(500);
+      xSemaphoreGive(i2c_mutex);
+      break;
+    }
+  }
+
+  delay(100);
+
+  while (true)
+  {
+    if (xSemaphoreTake(i2c_mutex, 50 / portTICK_PERIOD_MS))
+    {
+      uint8_t *p = readPosition(address);
+      pos[0] = p[0];
+      pos[1] = p[1];
+      xSemaphoreGive(i2c_mutex);
+      Serial.print("Adresa: ");
+      Serial.print(address);
+      Serial.print(": X - ");
+      Serial.print(pos[0]);
+      Serial.print(" Y - ");
+      Serial.println(pos[1]);
+      break;
+    }
+  }
+
+  Serial.println("");
+  Serial.println(lampCount);
+
+  while (true)
+  {
+    if (xSemaphoreTake(i2c_mutex, 1))
+    {
+      value = readTouch(address);
+      xSemaphoreGive(i2c_mutex);
+      if (value == 1 && ((lastValue != value) || ((millis() - onMillis) > (interval - 100))))
+      {
+        Serial.print(address);
+        Serial.print(": ");
+        Serial.println("dotyk!");
+        triggerCount = 0;
+        triggerX = pos[0];
+        triggerY = pos[1];
+        lastValue = value;
+      }
+      else if (value == 0 && lastValue != value)
+      {
+        Serial.print(address);
+        Serial.print(": ");
+        Serial.println("Konec dotyku!");
+        triggerCount = 0;
+        triggerX = pos[0];
+        triggerY = pos[1];
+        lastValue = value;
+      }
+    }
+
+    if (triggerCount != lampCount)
+    {
+      triggerCount++;
+      double rad = ((triggerX - pos[0]) * (triggerX - pos[0])) + ((triggerY - pos[1]) * (triggerY - pos[1]));
+      rad = sqrt(rad);
+      Serial.print(address);
+      Serial.print(" radius : ");
+      Serial.println(String(rad).c_str());
+
+      if (rad <= radius)
+      {
+        if (!on)
+        {
+          on = true;
+          if (xSemaphoreTake(i2c_mutex, 100 / portTICK_PERIOD_MS))
+          {
+            writePWM(address, high);
+            xSemaphoreGive(i2c_mutex);
+          }
+        }
+        onMillis = millis();
+      }
+    }
+
+    if (on && (millis() - onMillis) > interval)
+    {
+      if (xSemaphoreTake(i2c_mutex, 100 / portTICK_PERIOD_MS))
+      {
+        writePWM(address, low);
+        xSemaphoreGive(i2c_mutex);
+      }
+      on = false;
+    }
+  }
+  vTaskDelete(NULL);
+}
+
+void sensors(void *parameters)
+{
+  while (true)
+  {
+    if (xSemaphoreTake(i2c_mutex, (50 / portTICK_PERIOD_MS)))
+    {
+      Serial.println("");
+      Serial.print("Teplota: ");
+      Serial.println(String(bme.readTemperature()).c_str());
+      Serial.print("Tlak: ");
+      Serial.println(String(bme.readPressure()).c_str());
+      Serial.print("Vlhkost: ");
+      Serial.println(String(bme.readHumidity()).c_str());
+      Serial.print("Viditelné světlo: ");
+      Serial.println(uv.readVisible());
+      Serial.print("Infra-red: ");
+      Serial.println(uv.readIR());
+      xSemaphoreGive(i2c_mutex);
+      delay(1000);
+    }
+  }
+}
+
+void setup()
+{
+  // put your setup code here, to run once:
+  delay(700);
+
+  Serial.begin(230400);
+  Wire.begin(22, 23); //ESP32 bez LoRa
+  bme.begin(0x76);
+  uv.begin();
+
+  Serial.println("");
+  Serial.println("");
+
+  for (int i = 4; i < 20; i++)
+  {
+    xTaskCreatePinnedToCore(lamp, "lamp", 10000, (void *)i, 3, NULL, 1);
+  }
+
+  xTaskCreatePinnedToCore(sensors, "sensory", 10000, NULL, 3, NULL, 1);
+
+  vTaskDelete(NULL);
+}
+
+void loop()
+{
+  vTaskDelete(NULL);
+}
+
+
+
+
+// Stary kod s fajn features k fronte ze ktereho se da neco vzit nez se odmazou zbytecnosti uplne
+/*
+#############################################################################################################################
 // TODO definovat jak velke okoli lampa ovlivnuje
 #define PERIMETR 5
 // Inicializace fronty pro ukladani souradnic
 QueueHandle_t queueX = xQueueCreate(127, sizeof(uint8_t));
 QueueHandle_t queueY = xQueueCreate(127, sizeof(uint8_t));
 SemaphoreHandle_t i2c_mutex = xSemaphoreCreateMutex();
-Adafruit_BME280 bme;
-Adafruit_SI1145 uv = Adafruit_SI1145();
 
 unsigned long sensorMillis = 0;
 unsigned long onMillis = 0;
@@ -32,11 +273,7 @@ boolean turnOn = false;
 void lightradius(uint8_t x, uint8_t y, uint8_t lastValue, int address) 
 {
     // TODO implementace semaforu? (=> jistota ze check probehne kompletne ALE docela ztrata vyhody multitasku)
-    Serial.print("Lampa: ");
-    Serial.println(address);
-    Serial.print("Last stav: ");
-    Serial.println(lastValue);
-    Serial.println("So Far So Good.");
+    
   /*
     // Podminka zda ma vubec smysl neco delat
     if((uxQueueMessagesWaiting(queueX) != 0) && (uxQueueMessagesWaiting(queueX) == uxQueueMessagesWaiting(queueY)))
@@ -101,7 +338,7 @@ void lightradius(uint8_t x, uint8_t y, uint8_t lastValue, int address)
         Serial.println("Something went wrong in the queue :(");
     }
 }
-*/
+
 }
 void koko(void *parameters)
 {
@@ -182,7 +419,7 @@ void koko(void *parameters)
           else 
           {
             Serial.println("Chyba, Y neulozeno.");
-          }*/
+          }
         }
         lightradius(coorX, coorY, lastValue, address);
       }
@@ -200,43 +437,4 @@ void koko(void *parameters)
     vTaskDelay(2);
     delay(20);
 }
-  
-
-
-void setup()
-{
-  //Nastavíme I2C sběrnici
-
-  delay(700);
-  //Wire.begin(4, 15); //ESP32 s LoRou
-  Wire.begin(22, 23); //ESP32 bez LoRa
-
-  writeMode(5, 1);
-  writeMode(6, 1);
-  writeMode(7, 1);
-  // Nahrani souradnic do ATTiny
-  writePosition(5, 55, 15);
-  writePosition(6, 66, 16);
-  writePosition(7, 77, 17);
-  
-  bme.begin(0x76);
-  uv.begin();
-  pinMode(2, OUTPUT);
-
-  //inicializujeme display
-
-  Serial.begin(115200);
-  Serial.println("");
-
-  for (int i = 1; i < 50; i++)
-  {
-    xTaskCreatePinnedToCore(koko, "lamp", 10000, (void *)i, 3, NULL, 1);
-    delayMicroseconds(200);
-  }
-  vTaskDelete(NULL);
-}
-
-void loop()
-{
-  vTaskDelete(NULL);
-}
+*/
